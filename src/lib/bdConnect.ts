773e6e16bf8b2454cf9f17e1b5b421471bf4072a
Habibur Rahman <habiburrahmanofficial157@gmail.com>
2024-06-10 21:18:56 +0600

    database design

--- src/lib/bdConnect.ts
+++ src/lib/bdConnect.ts
@@ -5,7 +5,20 @@ type connectionObject = {
 };
 const connection: connectionObject = {};
 async function dbConnect(): Promise<void> {
-    if(connection.isConnectd){
-        console.log("Already connected to database")
-    }
+  if (connection.isConnectd) {
+    console.log("Already connected to database");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(process.env.MONGOBD_URI || "", {});
+    // console.log(db);
+    connection.isConnectd = db.connections[0].readyState;
+    console.log("DB Connected Successfully");
+  } catch (error) {
+    console.log("DB Connected failed", error);
+
+    process.exit(1);
+  }
 }
+
+export default dbConnect;
