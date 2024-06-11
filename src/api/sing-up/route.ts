2a8f173cd8817faaf593d3a1f00f4b3e72885960
Habibur Rahman <habiburrahmanofficial157@gmail.com>
2024-06-12 02:04:35 +0600

    sing up page design and code

--- src/api/sing-up/route.ts
+++ src/api/sing-up/route.ts
@@ -24,10 +24,54 @@ export async function POST(request: Request) {
 
     const existingUserByEmail = await UserModel.findOne({
       email,
-    })
+    });
+
+    const verifyCode = Math.floor(10000 + Math.random() * 90000).toString;
+
     if (existingUserByEmail) {
-        true //todu back here
+      true; //todu back here
+    } else {
+      const hasedPassword = await bcrypt.hash(password, 10);
+
+      const expiryDate = new Date();
+
+      expiryDate.setHours(expiryDate.getHours() + 1);
+
+      const newUser = new UserModel({
+        username,
+        password: hasedPassword,
+        verifyCode,
+        verifyCodeExpiry: expiryDate,
+        isverified: false,
+        isAccepttingMessage: true,
+        messages: [],
+      });
+      await newUser.save();
     }
+    // send farification email
+    const emailResponse = await sendVerificationEmail(
+      email,
+      username,
+      verifyCode
+    );
+
+    if (!emailResponse.success) {
+      return Response.json(
+        {
+          succuss: false,
+          message: emailResponse.message,
+        },
+        { status: 500 }
+      );
+    }
+
+    return Response.json(
+      {
+        succuss: true,
+        message: "Users Registered successfully. Please verify your email",
+      },
+      { status: 201 }
+    );
   } catch (error) {
     console.error("Error registering user", error);
     return Response.json(
