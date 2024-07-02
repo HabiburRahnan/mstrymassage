90807433dae5b5e9f4a904c18ae1dcd23a573740
Habibur Rahman <habiburrahmanofficial157@gmail.com>
2024-07-03 01:09:22 +0600

    db connect and user cheked

--- src/api/auth/options.ts
+++ src/api/auth/options.ts
@@ -1,22 +1,47 @@
-import { NextAuthOptions } from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
-import bcrypt from "bcryptjs"
-import bdConnect from "@/lib/bdConnect"
-import UserModel from "@/model/User"
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import bdConnect from "@/lib/bdConnect";
+import UserModel from "@/model/User";
+import dbConnect from "@/lib/bdConnect";
 
-export const authOptions: NextAuthOptions ={
-providers:[
+export const authOptions: NextAuthOptions = {
+  providers: [
     CredentialsProvider({
-        id:"credentials",
-        name: "Credentials",
-        credentials:{
-            username:{label:"Email", type:"Text", },
-            password:{label:"Password", type:"Password"}
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "Text" },
+        password: { label: "Password", type: "Password" },
+      },
+      async authorize(credentials: any): Promise<any> {
+        await dbConnect();
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifier },
+              { username: credentials.identifier },
+            ],
+          });
+          if (!user) {
+            throw new Error("No user found with this email");
+          }
+          if (user.isverified) {
+            throw new Error("please verify your acount before login");
+          }
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+          if (isPasswordCorrect) {
+            return user;
+          } else {
+            throw new Error("Incorrect Password");
+          }
+        } catch (err: any) {
+          throw new Error(err);
         }
-        async authorize(credentials:any):Promise<any>{
-
-        }
-
-    })
-]
-} 
+      },
+    }),
+  ],
+};
